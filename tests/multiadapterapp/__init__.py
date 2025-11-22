dd7f6e2d1269ea9728ce47b70c729a4153acf3f6
Timur Kady <kadyevtr@gmail.com>
2025-11-22 20:04:10 +0300

    Add integration coverage for boot adapters

---
+++ tests/multiadapterapp/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""multiadapterapp
+
+Application package used to verify BootManager adapter selection."""
+
+from __future__ import annotations
+
+from .app import MultiAdapterAdmin, MultiAdapterAppConfig, default
+
+__all__ = ["MultiAdapterAdmin", "MultiAdapterAppConfig", "default"]
+
+
+# The End
